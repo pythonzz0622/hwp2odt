c8e73f17289ad7690ea287490d0104076a5cec79
mac <pythonzz0622@gmail.com>
2022-08-10 17:19:13 +0900

    add paraser

--- source/hwp2odt.py
+++ source/hwp2odt.py
@@ -1,10 +1,19 @@
 import os
 import cloudconvert
+import argparse
 
 # set api key
 cloudconvert.configure(api_key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZmEzNzYwYWIxZjA1NDM2N2FhY2IwZWQ5NjU3OGViZjEyZjcxZmViYTJjYzYyNzA2ODM5NGRmMzMzNjJhYmJiZDhlZTFiNjkyNTliZDYzOGYiLCJpYXQiOjE2NjAwNDQ4MjguNzI2NjQsIm5iZiI6MTY2MDA0NDgyOC43MjY2NDIsImV4cCI6NDgxNTcxODQyOC43MTQzMiwic3ViIjoiNTkyMjI1MjMiLCJzY29wZXMiOlsidXNlci5yZWFkIiwidGFzay5yZWFkIiwidXNlci53cml0ZSIsInRhc2sud3JpdGUiLCJ3ZWJob29rLnJlYWQiLCJ3ZWJob29rLndyaXRlIiwicHJlc2V0LnJlYWQiLCJwcmVzZXQud3JpdGUiXX0.A05yFRwzrZWLFFoEIDcNQLqzQrwy4PTfG-X46ZShDrtden_VzgSaXJqCdrP0Ep_E-2Yihgdv90b-bNbP8J2AfJwsibf-L4CcSfAZaU9e7fXDQYfjg6jTTstAbkBAk5ewIKEA3xa-srVt518vs_Ngkc3RMYnWycNP5fM7IMnqL7MB5yfkXSkCPC1hq6Rtf9B9VbmG4bFy7F8Y-VqMtYLrqZHN0D4FV3AfHBC6HUyVEX997eV4StMKaINNvieSSNrPIkUM5dWmktD7NgZZ4iUpUorlFuTPZhxaFotFid37-v6r4N68wj6If3-hnzADe4zrV7l6Eku9wYSt4CxTqIjeQPnvtCq9dlPTYom-OjrjPIczjD0woHbm1dS1Jw3VMaU4j5blVii2EYBtLLxRw6D16quI__Xd6vYWCbPucaEI-d4eaQSr42BRTEtohSbwefEVwYAugYj3JYkQxjHmgVXGiMWM8EhX0FbFjuxmgo5J6iz3jngX_gn8CqBv-Rrec7m0J-ef1zywpSpemW0iRdaFiDcuJpzrGtmSjgR8_ZAA_vTH2P-HFzGyh9zOF1TTL6uLs_oEPMQewrJvScaZLxJCOILWZKW-V86vSfaI-saXoAt0mLpgZ893F4J8E-np5_wOIy32VFOnh7Ce64_eG48IeW-Ojftu0_Hx_prmE76MPxs' , sandbox = False)
-
 ip_address = '218.48.227.56'
+
+parser = argparse.ArgumentParser(description='get config')
+parser.add_argument('--file_name', type=str, help='file_name')
+parser.add_argument('--output_dir', type=str, help='output_dir')
+
+args = parser.parse_args()
+file_name = args.file_name
+output_dir = args.output_dir
+
 def hwp2odt(file_name , output_dir):
     '''
     Description:
@@ -56,4 +65,4 @@ def hwp2odt(file_name , output_dir):
     return None
 
 if __name__ == "__main__":
-    hwp2odt("test.hwp" , 'output/')
+    hwp2odt(file_name , output_dir)
