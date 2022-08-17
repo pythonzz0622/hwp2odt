58f5311ca11e66b5b3a30097a727dca15d7f12bb
mac <pythonzz0622@gmail.com>
2022-08-17 17:30:48 +0900

    add run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-python source/hwp2odt.py --file_name="혁신추진상황.hwp" --output_dir="output"
-python source/hwp2odt.py --file_name="향후추진계획.hwp" --output_dir="output"
-python source/hwp2odt.py --file_name="향후추진계획위원회.hwp" --output_dir="output"
-python source/hwp2odt.py --file_name="정보공개청구서.hwp" --output_dir="output"
+#!/bin/bash
+
+for entry in `ls ./dataset/`; do
+    python source/hwp2odt.py --file_name=$entry --output_dir="output"
+done
